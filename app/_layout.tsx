f42f8d236dbb59409624ad2e14ae0e347e9d6f68
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2025-10-24 23:08:34 +0530

    crop hsitory tab fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,23 +2,23 @@ import Navbar from "@/src/components/Navbar";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "../src/contexts/AuthContext";
-
+import { CropProvider } from "../src/contexts/CropContext";
 
 export default function RootLayout() {
   return (
-        <AuthProvider>
-    
-    <SafeAreaProvider>
-      <Stack
-        screenOptions={{
-          headerShown: true,
-          header: () => <Navbar />,
-          navigationBarHidden:true,
-          contentStyle: { backgroundColor: "#ECF39E" }, 
-        }}
-      />
-    </SafeAreaProvider>
+    <AuthProvider>
+      <CropProvider>
+        <SafeAreaProvider>
+          <Stack
+            screenOptions={{
+              headerShown: true,
+              header: () => <Navbar />,
+              navigationBarHidden: true,
+              contentStyle: { backgroundColor: "#ECF39E" },
+            }}
+          />
+        </SafeAreaProvider>
+      </CropProvider>
     </AuthProvider>
-
   );
 }
